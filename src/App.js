2de00f363ac493fd8dbea4aa7c15656ffbf54d1a
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-03-27 18:44:21 -0600

    import SearchBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;
