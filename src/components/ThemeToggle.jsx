5da8b501cf7fc018294c040ed76da55227a371f4
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-03-30 16:45:15 -0600

    add logic to toggle between dark and light mode

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { HiSun, HiMoon } from "react-icons/hi";
+import { ThemeContext } from "../context/ThemeContext";
 
 function ThemeToggle() {
-  return <div>ThemeToggle</div>;
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return (
+    <div>
+      {theme === "dark" ? (
+        <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+          <HiSun /> Light Mode
+        </div>
+      ) : (
+        <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+          <HiMoon /> Dark Mode
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ThemeToggle;
