a7f1c1efae46390426ad05db01365f7ffc1cb87a
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-03-30 16:56:25 -0600

    Refactor and add comments to code

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -2,28 +2,34 @@ import React, { useContext } from "react";
 import { HiSun, HiMoon } from "react-icons/hi";
 import { ThemeContext } from "../context/ThemeContext";
 
-function ThemeToggle() {
+const ThemeToggle = () => {
+  // Use the useContext hook to access the theme and setTheme values from ThemeContext
   const { theme, setTheme } = useContext(ThemeContext);
 
+  // Determine if the current theme is dark or not
+  const isDarkMode = theme === "dark";
+
+  // Define a function to toggle the theme between dark and light
+  const toggleTheme = () => setTheme(isDarkMode ? "light" : "dark");
+
+  // Define the icon to be displayed based on the current theme
+  const icon = isDarkMode ? (
+    <HiSun className="text-primary text-2xl mr-2" />
+  ) : (
+    <HiMoon className="text-primary text-2xl mr-2" />
+  );
+
+  // Define the label to be displayed based on the current theme
+  const label = isDarkMode ? "Light Mode" : "Dark Mode";
+
+  // Render the component with the icon, label, and click handler
   return (
     <div className="p-2">
-      {theme === "dark" ? (
-        <div
-          className="flex items-center cursor-pointer"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          <HiSun className="text-primary text-2xl mr-2" /> Light Mode
-        </div>
-      ) : (
-        <div
-          className="flex items-center cursor-pointer"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          <HiMoon className="text-primary text-2xl mr-2" /> Dark Mode
-        </div>
-      )}
+      <div className="flex items-center cursor-pointer" onClick={toggleTheme}>
+        {icon} {label}
+      </div>
     </div>
   );
-}
+};
 
 export default ThemeToggle;
