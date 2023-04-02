48e0ac395976ba5fbf2d770fd7a6915f2803577e
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-01 22:31:54 -0600

    add tailwind styles to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,24 +5,31 @@ import { AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
   return (
-    <div>
+    <div className="rounded-div flex items-center justify-between h-20 font-bold">
       <Link to="/">
-        <h1>EZRecipes</h1>
+        <h1 className="text-2xl">EZRecipes</h1>
       </Link>
-      <div>
+      <div className="hidden md:block">
         <ThemeToggle />
       </div>
-      <div>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signup">Sign Up</Link>
+      <div className="hidden md:block">
+        <Link to="/signin" className="p-4 hover:text-accent">
+          Sign In
+        </Link>
+        <Link
+          to="/signup"
+          className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg"
+        >
+          Sign Up
+        </Link>
       </div>
       {/* Menu Icon */}
-      <div>
+      <div className="block md:hidden cursor pointer z-10">
         <AiOutlineMenu />
       </div>
       {/* Mobile Menu */}
-      <div>
-        <ul>
+      <div className="md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10">
+        <ul className="w-full p-4">
           <li>
             <Link to="/Home"></Link>
           </li>
