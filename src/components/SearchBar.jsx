987af5aba2fd6931f12fcd4bad7255636f41d925
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-02 18:09:07 -0600

    Update SearchBar.jsx
    - Create search bar and button
    - when user clicks on search button makes an api request
    - at the moment returning data as a console.log

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function SearchBar() {
-  return <div>SearchBar</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(searchTerm);
+
+    const options = {
+      method: "GET",
+      url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch",
+      params: {
+        query: searchTerm,
+      },
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+        "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div className="rounded-div flex">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Find Recipes"
+        />
+        <button
+          className="w-[50%] my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
