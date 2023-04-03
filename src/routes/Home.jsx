495fb7ea3361a41e3adf98d7ac7f85af94347507
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-02 18:09:29 -0600

    import SearchBar and add SearchBar to Home component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SearchBar from "../components/SearchBar";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <SearchBar />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
