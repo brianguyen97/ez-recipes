2c78d69959fa7976144377b1f7240d43de193ee8
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-04 15:53:45 -0600

    Update RecipesContainer.jsx
    - import RecipesContainerItem
    - destructure results from props
    - map through results from api to dynamically render recipes based on the search query
    - currently static data on the RecipesContainerItem

--- src/components/RecipesContainer.jsx
+++ src/components/RecipesContainer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import RecipesContainerItem from "./RecipesContainerItem";
 
 function RecipesContainer(props) {
+  const { results } = props;
+
   return (
-    <div className="rounded div grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4"></div>
+    <div className="mt-5 rounded-div grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      {results.length !== 0 && // Use conditional rendering to only render when results array is not empty
+        results.map((recipe) => (
+          <RecipesContainerItem key={recipe.id} {...recipe} />
+        ))}
+    </div>
   );
 }
 
