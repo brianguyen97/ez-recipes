0a6559cc1cea95a71fac3ddad6e4615149affb59
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-05 19:09:39 -0600

    Generate unique id for each recipe rendered

--- src/components/RecipesContainer.jsx
+++ src/components/RecipesContainer.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useId } from "react";
 import RecipesContainerItem from "./RecipesContainerItem";
+import { v4 as uuidv4 } from "uuid";
 
 function RecipesContainer(props) {
   const { results } = props;
@@ -8,7 +9,7 @@ function RecipesContainer(props) {
     <div className="mt-5 rounded-div grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
       {results.length !== 0 && // Use conditional rendering to only render when results array is not empty
         results.map((recipe) => (
-          <RecipesContainerItem key={recipe.id} {...recipe} />
+          <RecipesContainerItem key={uuidv4()} {...recipe} />
         ))}
     </div>
   );
