2e54f1acebc7a3990577d13c2099c74d0bd1decf
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-06 14:10:05 -0600

    Update SpecificRecipe.jsx
    - Make a single API call using useEffect upon loading a single recipe
    - Currently returning results in a console.log

--- src/routes/SpecificRecipe.jsx
+++ src/routes/SpecificRecipe.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+// Defining constants for the API key, host, and URL
+const API_KEY = process.env.REACT_APP_RAPID_API_KEY;
+const API_HOST = process.env.REACT_APP_RAPID_API_HOST;
 
 function SpecificRecipe() {
+  const { RecipeId } = useParams();
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${RecipeId}/information`,
+      headers: {
+        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Host": API_HOST,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
   return <div>SpecificRecipe</div>;
 }
 
