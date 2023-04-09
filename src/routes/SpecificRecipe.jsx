e488345a0cd8c0497848c98251b2aac3620cf277
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-09 12:48:23 -0600

    Update SpecificRecipe.jsx
    - import useState for object from API
    - API was returning unsanitized html, implement parser
    - render recipe title and summary dynamically
    - add reviews and ratings (hard coded)
    - add comments for readability

--- src/routes/SpecificRecipe.jsx
+++ src/routes/SpecificRecipe.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-// Defining constants for the API key, host, and URL
-const API_KEY = process.env.REACT_APP_RAPID_API_KEY;
-const API_HOST = process.env.REACT_APP_RAPID_API_HOST;
+const API_KEY = process.env.REACT_APP_RAPID_API_KEY; // Define API key constant
+const API_HOST = process.env.REACT_APP_RAPID_API_HOST; // Define API host constant
 
 function SpecificRecipe() {
-  const { RecipeId } = useParams();
+  const { RecipeId } = useParams(); // Retrieve RecipeId from URL parameter
+  const [recipeData, setRecipeData] = useState(null); // Initialize recipeData state to null
+
+  const parser = new DOMParser(); // Create new instance of DOMParser to parse HTML string
 
   useEffect(() => {
     const options = {
@@ -22,14 +24,33 @@ function SpecificRecipe() {
     axios
       .request(options)
       .then(function (response) {
-        console.log(response.data);
+        setRecipeData(response.data); // Update recipeData state with API response data
+        console.log(response.data); // Log API response data to console for debugging purposes
       })
       .catch(function (error) {
-        console.error(error);
+        console.error(error); // Log any errors to console for debugging purposes
       });
-  }, []);
+  }, []); // Run useEffect hook only once when component mounts
 
-  return <div>SpecificRecipe</div>;
+  return (
+    <div className="rounded-div">
+      {/* Render recipe title if recipeData is not null, otherwise show loading message */}
+      {recipeData ? <h2>{recipeData.title}</h2> : <p>Loading...</p>}
+
+      {/* Render rating and review count */}
+      <div>5 Stars | 15 Reviews |</div>
+
+      {/* Render recipe summary if recipeData is not null */}
+      {recipeData ? (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: parser.parseFromString(recipeData.summary, "text/html").body
+              .innerHTML,
+          }}
+        />
+      ) : null}
+    </div>
+  );
 }
 
 export default SpecificRecipe;
