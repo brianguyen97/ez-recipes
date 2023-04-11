6eb527462342745f4f5ce83a0da4bc67ed7597c4
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-10 18:01:11 -0600

    move prepMins, cookMins and servings to seperate component

--- src/components/CookingTime.jsx
+++ src/components/CookingTime.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-function CookingTime() {
-  return <div>CookingTime</div>;
+function CookingTime(props) {
+  return (
+    <div className="grid grid-cols-3 gap-2 p-3 bg-gray-200 rounded-lg mt-5">
+      <div className="bg-white rounded-md p-2 text-center">Prep Mins: 30</div>
+      <div className="bg-white rounded-md p-2 text-center">Cook Mins: 60</div>
+      <div className="bg-white rounded-md p-2 text-center">Servings: 4</div>
+    </div>
+  );
 }
 
 export default CookingTime;
