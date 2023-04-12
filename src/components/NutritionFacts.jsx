0b020af562edaa71cac4e6a6d0c754a647da5191
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-12 17:50:35 -0600

    Update NutritionFacts.jsx
    - Create the nutrition facts component
    - Add styling
    - currently hard coded values

--- src/components/NutritionFacts.jsx
+++ src/components/NutritionFacts.jsx
@@ -1,7 +1,59 @@
 import React from "react";
 
 function NutritionFacts() {
-  return <div>NutritionFacts</div>;
+  const servingSize = "1 bowl (240g)";
+  const calories = "290";
+  const carbs = "35g";
+  const fat = "12g";
+  const protein = "10g";
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-yellow-100 rounded-lg p-5 shadow-md">
+      <p className="text-lg font-medium text-gray-700 mb-3 text-center">
+        Nutrition Facts
+      </p>
+      <table className="w-full text-center">
+        <tbody>
+          <tr>
+            <td className="font-medium text-gray-700 py-1 border-b border-gray-300">
+              Serving Size
+            </td>
+            <td className="text-gray-600 py-1 border-b border-gray-300">
+              {servingSize}
+            </td>
+          </tr>
+          <tr>
+            <td className="font-medium text-gray-700 py-1 border-b border-gray-300">
+              Calories
+            </td>
+            <td className="text-gray-600 py-1 border-b border-gray-300">
+              {calories}
+            </td>
+          </tr>
+          <tr>
+            <td className="font-medium text-gray-700 py-1 border-b border-gray-300">
+              Total Carbs
+            </td>
+            <td className="text-gray-600 py-1 border-b border-gray-300">
+              {carbs}
+            </td>
+          </tr>
+          <tr>
+            <td className="font-medium text-gray-700 py-1 border-b border-gray-300">
+              Total Fat
+            </td>
+            <td className="text-gray-600 py-1 border-b border-gray-300">
+              {fat}
+            </td>
+          </tr>
+          <tr>
+            <td className="font-medium text-gray-700 py-1">Protein</td>
+            <td className="text-gray-600 py-1">{protein}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default NutritionFacts;
