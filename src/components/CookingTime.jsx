6beb37f8d8be0e4dcf44aa93f56f639f62a6a859
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-12 17:38:07 -0600

    implement optional chaining

--- src/components/CookingTime.jsx
+++ src/components/CookingTime.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-function CookingTime(props) {
+function CookingTime({ recipeData }) {
   return (
     <div className="grid grid-cols-3 gap-2 p-3 bg-gray-200 rounded-lg mt-5">
       <div className="bg-white rounded-md p-2 text-center">
-        Prep Mins: {props.recipeData.preparationMinutes}
+        Prep Mins: {recipeData?.preparationMinutes || "N/A"}
       </div>
       <div className="bg-white rounded-md p-2 text-center">
-        Cook Mins: {props.recipeData.cookingMinutes}
+        Cook Mins: {recipeData?.cookingMinutes || "N/A"}
       </div>
       <div className="bg-white rounded-md p-2 text-center">
-        Servings: {props.recipeData.servings}
+        Servings: {recipeData?.servings || "N/A"}
       </div>
     </div>
   );
