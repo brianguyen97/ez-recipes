92e14d919ea4b191e6f485e70af9928fd515e3b8
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-15 19:23:17 -0600

    Update Ingredients.jsx
    remove hard coded values, dynamically render the ingredients list

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 
-function Ingredients() {
-  const ingredients = [
-    "1 pound elbow macaroni",
-    "4 tablespoons unsalted butter",
-    "1/4 cup all-purpose flour",
-    "3 cups whole milk",
-    "1/2 teaspoon garlic powder",
-    "1/2 teaspoon onion powder",
-    "1/2 teaspoon mustard powder",
-    "1/4 teaspoon cayenne pepper",
-    "8 ounces grated cheddar cheese",
-    "4 ounces grated Gruyere cheese",
-    "4 ounces cream cheese, softened",
-    "1/2 cup panko bread crumbs",
-    "2 tablespoons chopped fresh parsley",
-    "Salt and pepper, to taste",
-  ];
+function Ingredients(props) {
+  // Log props.ingredients to console for debugging purposes
+  console.log(props.ingredients);
 
+  // Check if props.ingredients is undefined or null
+  if (!props.ingredients) {
+    // Return null to avoid rendering anything
+    return null;
+  }
+
+  // Render the ingredients list
   return (
-    <div className="bg-white rounded-lg p-5 shadow-md mt-5">
+    <div className="bg-white p-5 mt-5 mx-auto border-b-2">
       <p className="text-2xl font-medium text-gray-700 mb-3">Ingredients</p>
-      <ul className="list-disc pl-0 text-gray-600 leading-relaxed">
-        {ingredients.map((ingredient, index) => (
+      <ul className="list-disc pl-0 text-gray-600 leading-relaxed ">
+        {/* Map through each ingredient in props.ingredients */}
+        {props.ingredients.map((ingredient, index) => (
           <li key={index} className="flex">
             <span className="mr-2">&#8226;</span>
-            <span className="">{ingredient}</span>
+            <span className="">
+              {/* Round the amount property of the ingredient to the nearest ten */}
+              {Math.round(ingredient?.measures.metric.amount / 10) * 10}{" "}
+              {/* Use the unitShort property of the metric object */}
+              {ingredient?.measures.metric.unitShort} {ingredient?.name}
+            </span>
           </li>
         ))}
       </ul>
