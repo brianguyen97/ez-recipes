d31e164dae45092e8ca899f84157c1d1a63b1b51
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-25 12:46:23 -0600

    add comments, remove console log

--- src/components/RecipeInstructions.jsx
+++ src/components/RecipeInstructions.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function RecipeInstructions({ analyzedInstructions, instructions }) {
+  // If there are no analyzed instructions, don't render anything
   if (!analyzedInstructions) return null;
 
+  // Get the list of steps from the analyzed instructions
   const data = analyzedInstructions[0].steps;
-  console.log(data, "data");
 
+  // Loop through each step and display it in a list item
   return (
     <ol className="list-inside">
       {data.map((step) => (
-        <li key={uuidv4()} className="mb-4">
-          <p className="text-lg font-medium">{step.number}</p>
+        // Use the uuidv4 package to generate a unique key for each list item
+        <li key={uuidv4()} className="mb-4 flex items-center">
+          {/* Display the step number followed by a dot */}
+          <div className="mr-2 text-lg font-medium">{step.number}.</div>
+          {/* Display the step text */}
           <p className="text-gray-700">{step.step}</p>
         </li>
       ))}
