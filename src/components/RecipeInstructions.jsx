a91f2ccaeec216431849f02278f28613c4476f7e
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-25 12:52:44 -0600

    add padding & conditional to prevent errors

--- src/components/RecipeInstructions.jsx
+++ src/components/RecipeInstructions.jsx
@@ -4,13 +4,14 @@ import { v4 as uuidv4 } from "uuid";
 function RecipeInstructions({ analyzedInstructions, instructions }) {
   // If there are no analyzed instructions, don't render anything
   if (!analyzedInstructions) return null;
+  if (!instructions) return null;
 
   // Get the list of steps from the analyzed instructions
   const data = analyzedInstructions[0].steps;
 
   // Loop through each step and display it in a list item
   return (
-    <ol className="list-inside">
+    <ol className="list-inside p-4 md:p-6 lg:p-8">
       {data.map((step) => (
         // Use the uuidv4 package to generate a unique key for each list item
         <li key={uuidv4()} className="mb-4 flex items-center">
