33641fa08520919cfcbdad1f82eb9b47dc16d428
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-25 12:27:23 -0600

    add conditional to prevent api errors

--- src/components/RecipeInstructions.jsx
+++ src/components/RecipeInstructions.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 function RecipeInstructions({ analyzedInstructions, instructions }) {
+  if (!analyzedInstructions) return null;
+
   console.log(
     analyzedInstructions[0].steps,
     instructions,
