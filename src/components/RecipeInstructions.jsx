3e124e15b8d8182498e01d9a144c83990ab9e36d
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-25 12:24:35 -0600

    destructure props, console log for debugging

--- src/components/RecipeInstructions.jsx
+++ src/components/RecipeInstructions.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-function RecipeInstructions() {
+function RecipeInstructions({ analyzedInstructions, instructions }) {
+  console.log(
+    analyzedInstructions[0].steps,
+    instructions,
+    "from reccipeInsturctions"
+  );
   return <div>RecipeInstructions</div>;
 }
 
