e9ea17ae3c2acb301e94a8d0e1a1cffe213aafbc
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-25 12:43:52 -0600

    render instructions dynamically

--- src/components/RecipeInstructions.jsx
+++ src/components/RecipeInstructions.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function RecipeInstructions({ analyzedInstructions, instructions }) {
   if (!analyzedInstructions) return null;
 
-  console.log(
-    analyzedInstructions[0].steps,
-    instructions,
-    "from reccipeInsturctions"
+  const data = analyzedInstructions[0].steps;
+  console.log(data, "data");
+
+  return (
+    <ol className="list-inside">
+      {data.map((step) => (
+        <li key={uuidv4()} className="mb-4">
+          <p className="text-lg font-medium">{step.number}</p>
+          <p className="text-gray-700">{step.step}</p>
+        </li>
+      ))}
+    </ol>
   );
-  return <div>RecipeInstructions</div>;
 }
 
 export default RecipeInstructions;
