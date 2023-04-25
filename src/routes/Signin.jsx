c3de8a4ed3a3791705f7f86454e8fc3c99dae95c
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-25 14:10:58 -0600

    add styling to signin

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -3,42 +3,64 @@ import { useState } from "react";
 function Signin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showError, setShowError] = useState(false);
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setErrorMessage("Please fill in all fields");
+      setShowError(true);
+    } else {
+      // code to handle login
+    }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email</label>
+    <div className="flex justify-center items-center h-screen">
+      <form
+        className="bg-white p-8 rounded-lg shadow-lg"
+        onSubmit={handleLogin}
+      >
+        {showError && <p className="text-red-500 mb-4"> {errorMessage} </p>}
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+            {" "}
+            Email{" "}
+          </label>
           <input
             type="text"
             id="email"
+            name="email"
             value={email}
-            onChange={handleEmailChange}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full px-3 py-2 border border-gray-400 rounded"
           />
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            {" "}
+            Password{" "}
+          </label>
           <input
             type="password"
             id="password"
+            name="password"
             value={password}
-            onChange={handlePasswordChange}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full px-3 py-2 border border-gray-400 rounded"
           />
         </div>
-        <button type="submit">Sign In</button>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          {" "}
+          Sign In{" "}
+        </button>
       </form>
     </div>
   );
