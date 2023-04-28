ac8f1553f0addde76449fd9051c99051e2fd520e
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-28 17:55:15 -0600

    Update RecipeInstructions.jsx
    - make step and direction not inline
    - removed duplicate number

--- src/components/RecipeInstructions.jsx
+++ src/components/RecipeInstructions.jsx
@@ -12,11 +12,13 @@ function RecipeInstructions({ analyzedInstructions, instructions }) {
   return (
     <ol className="list-inside bg-gray-100 rounded-md p-4 md:p-6 lg:p-8">
       {data.map((step) => (
-        <li key={uuidv4()} className="mb-4 flex items-center">
+        <li key={uuidv4()} className="mb-4 flex items-start">
           <div className="w-8 h-8 mr-2 flex justify-center items-center bg-gray-300 rounded-full text-gray-700 font-bold">
             {step.number}.
           </div>
-          <p className="text-gray-700">{step.step}</p>
+          <div className="flex-1">
+            <p className="text-gray-700 font-bold mb-1">{step.step}</p>
+          </div>
         </li>
       ))}
     </ol>
