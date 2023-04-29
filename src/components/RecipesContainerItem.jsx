6f1381d0ed5f70110a4bd03a6c79dbbf965c45d6
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-28 18:27:02 -0600

    styling changes

--- src/components/RecipesContainerItem.jsx
+++ src/components/RecipesContainerItem.jsx
@@ -2,26 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function RecipesContainerItem(props) {
-  // Destructure the image, title, and id props from the props object
   const { image, title, id } = props;
 
   return (
-    <div className="rounded-div w-full max-w-xs mx-auto bg-white rounded-lg shadow-md overflow-hidden">
-      {/* Display the recipe image */}
-      <img
-        className="h-48 w-full object-contain"
-        src={image}
-        alt="Card image cap"
-      />
+    <div className="rounded-div w-full max-w-xs mx-auto bg-white rounded-lg shadow-md overflow-hidden transition duration-200 transform hover:-translate-y-1">
+      <img className="h-48 w-full object-contain" src={image} alt={title} />
       <div className="p-4">
-        {/* Display the recipe title */}
         <h3 className="text-gray-900 font-semibold text-lg text-center">
           {title}
         </h3>
-
-        {/* Create a link to view the recipe using the Link component */}
         <Link
-          to={`/recipe/${id}`} // Set the link URL dynamically using a template literal
+          to={`/recipe/${id}`}
           className="mt-2 text-lg font-medium text-indigo-500 hover:text-indigo-600 flex justify-center"
         >
           View Recipe
