6149dd7f28eb3b09f27923ebf3983a70a3422980
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-28 18:53:37 -0600

    fix typo

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -64,7 +64,7 @@ function Signup() {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
           {" "}
-          Sign In{" "}
+          Sign Up{" "}
         </button>
       </form>
     </div>
