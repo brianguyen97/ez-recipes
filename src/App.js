eb7ada40c562757476f0af09bd06cb240cfc00dc
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-04-28 18:28:03 -0600

    change home route

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Signup from "./routes/Signup";
 import Account from "./routes/Account";
 import RecipeSearchBasic from "./routes/RecipeSearchBasic";
 import SpecificRecipe from "./routes/SpecificRecipe";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <ThemeProvider>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/search" element={<RecipeSearchBasic />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
