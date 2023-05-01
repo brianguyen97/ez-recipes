cd4b37e9788f36765d2c5c232cd41a96a9d16b85
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-05-01 17:01:19 -0600

    redesign signup page to match signin

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,74 +1,91 @@
 import { useState } from "react";
-import GoogleSignUpButton from "../components/GoogleSignUpButton";
 
 function Signup() {
-  // Set initial states for email, password, error message, and showError flag
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [showError, setShowError] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  // Function to handle login
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // Check if email or password is empty
-    if (!email || !password) {
-      // Set error message and showError flag
-      setErrorMessage("Please fill in all fields");
-      setShowError(true);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password === confirmPassword) {
+      console.log(`Submitting email ${email} with password ${password}`);
     } else {
-      // Code to handle login
+      console.log("Passwords do not match");
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form
-        className="bg-white p-8 rounded-lg shadow-lg"
-        onSubmit={handleLogin}
-      >
-        {/* Show error message if showError is true */}
-        {showError && <p className="text-red-500 mb-4"> {errorMessage} </p>}
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
-            {" "}
-            Email{" "}
-          </label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-3 py-2 border border-gray-400 rounded"
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="password"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            {" "}
-            Password{" "}
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 border border-gray-400 rounded"
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          {" "}
-          Sign Up{" "}
-        </button>
-      </form>
-      <GoogleSignUpButton />
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <div className="max-w-md w-full mx-auto px-4">
+        <div className="text-3xl font-bold mb-6 text-center">Sign Up</div>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="confirmPassword"
+            >
+              Confirm Password
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="confirmPassword"
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 w-full sm:w-auto"
+              type="submit"
+            >
+              Sign Up
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Already have an account? Click here to sign in.
+            </a>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
