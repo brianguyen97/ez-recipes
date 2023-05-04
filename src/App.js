89112a0709ae53f8026239d07d77893837389828
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-05-04 15:45:45 -0600

    remove anything related to old nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import { ThemeProvider } from "./context/ThemeContext";
 import { Route, Routes } from "react-router-dom";
 import Signin from "./routes/Signin";
 import Signup from "./routes/Signup";
@@ -13,17 +11,14 @@ import { AuthContextProvider } from "./context/AuthContext";
 function App() {
   return (
     <AuthContextProvider>
-      <ThemeProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/search" element={<RecipeSearchBasic />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/recipe/:RecipeId" element={<SpecificRecipe />} />
-        </Routes>
-      </ThemeProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/search" element={<RecipeSearchBasic />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/recipe/:RecipeId" element={<SpecificRecipe />} />
+      </Routes>
     </AuthContextProvider>
   );
 }
