8bb15b0ad26478aff4bd396b6cf6ff5eff3929c6
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-05-04 16:05:51 -0600

    import new navbar

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import RecipeSearchBasic from "./routes/RecipeSearchBasic";
 import SpecificRecipe from "./routes/SpecificRecipe";
 import LandingPage from "./components/LandingPage";
 import { AuthContextProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <AuthContextProvider>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/search" element={<RecipeSearchBasic />} />
