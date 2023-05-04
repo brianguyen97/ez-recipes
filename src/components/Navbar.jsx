9835f130d243a43e96dfe7b8a9401732afbdba24
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-05-04 16:20:18 -0600

    Update Navbar.jsx
    - Add State to see if a user is authenticated or not
    - Display logout and account option if a user is authenticated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaUser, FaSignOutAlt } from "react-icons/fa";
 
 function Navbar() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  const handleLogout = () => {
+    // logic to log out the user goes here
+    setIsAuthenticated(false);
+  };
+
   return (
     <nav className="bg-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -19,18 +26,37 @@ function Navbar() {
             >
               <FaSearch className="h-5 w-5 text-gray-700" />
             </Link>
-            <Link
-              to="/signin"
-              className="py-2 px-4 rounded-lg text-gray-800 font-medium ml-4 hover:bg-gray-200"
-            >
-              Sign In
-            </Link>
-            <Link
-              to="/signup"
-              className="py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-lg text-white font-medium ml-2"
-            >
-              Sign Up
-            </Link>
+            {isAuthenticated ? (
+              <>
+                <Link
+                  to="/account"
+                  className="p-2 rounded-full bg-gray-200 hover:bg-gray-300 ml-4"
+                >
+                  <FaUser className="h-5 w-5 text-gray-700" />
+                </Link>
+                <button
+                  onClick={handleLogout}
+                  className="py-2 px-4 rounded-lg text-gray-800 font-medium ml-4 hover:bg-gray-200"
+                >
+                  Logout <FaSignOutAlt className="inline-block h-4 w-4 ml-2" />
+                </button>
+              </>
+            ) : (
+              <>
+                <Link
+                  to="/signin"
+                  className="py-2 px-4 rounded-lg text-gray-800 font-medium ml-4 hover:bg-gray-200"
+                >
+                  Sign In
+                </Link>
+                <Link
+                  to="/signup"
+                  className="py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-lg text-white font-medium ml-2"
+                >
+                  Sign Up
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
