3fba80b175ea0341298783058b9c447df432cc37
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-05-07 18:30:16 -0600

    Style Changes RecipeContainer(Item)

--- src/components/RecipesContainer.jsx
+++ src/components/RecipesContainer.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import RecipesContainerItem from "./RecipesContainerItem";
-import { v4 as uuidv4 } from "uuid";
 
 function RecipesContainer(props) {
   const { results } = props;
 
   return (
-    <div className="mt-5 rounded-div grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      {results.length !== 0 &&
-        results.map((recipe) => (
-          <RecipesContainerItem key={uuidv4()} {...recipe} />
-        ))}
+    <div className="flex flex-wrap justify-center mt-8 gap-8">
+      {results.map((recipe) => (
+        <RecipesContainerItem key={recipe.id} {...recipe} />
+      ))}
     </div>
   );
 }
