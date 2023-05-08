da2349a13c279e3bd290e3eb26d79212c944627a
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-05-07 18:29:28 -0600

    Update SearchBar.jsx
    Restyled SearchBar component with TailwindCSS to improve its design and usability. Specifically, updated the input and button styles to use a more modern and consistent look, with improved focus states and responsive sizing. The updated design should improve the overall user experience and make the component more visually appealing.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,14 +5,14 @@ function SearchBar(props) {
     <div className="flex justify-center items-center mt-6">
       <form className="flex items-center" onSubmit={props.handleSubmit}>
         <input
-          className="w-64 px-3 py-2 mr-2 bg-primary border border-input rounded-2xl shadow"
+          className="w-64 px-3 py-2 mr-2 bg-white border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
           type="text"
           placeholder="Find Recipes"
           value={props.searchTerm}
           onChange={props.handleChange}
         />
         <button
-          className="px-3 py-2 bg-button text-btnText rounded-2xl shadow"
+          className="px-4 py-2 bg-indigo-500 text-white rounded-full shadow-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
           type="submit"
         >
           Search
